3e2137bce3d9097768c1f223279dda0618f9fce6
ASH <123207638+ash-netizen286@users.noreply.github.com>
2025-08-16 07:14:32 +0300

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,22 @@
+pacman -Syu --noconfirm
+
+pacman -S --noconfirm wget curl
+pacman -S --noconfirm rofi
+pacman -S --noconfirm firefox
+pacman -S --noconfirm alacritty
+
+pacman -S --needed --noconfirm git base-devel
+
+pacman -S --noconfirm networkmanager
+systemctl enable NetworkManager
+
+pacman -S --noconfirm conky
+pacman -S --noconfirm polybar
+pacman -S --noconfirm feh
+pacman -S --noconfirm ranger
+pacman -S --noconfirm pulseaudio pvaucontrol
+pacman -S --noconfirm xorg xorg-xinit xorg-apps
+pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
+
+pacman -S --noconfirm lightdm lightdm-gtk-greeter
+systemctl enable lightdm
